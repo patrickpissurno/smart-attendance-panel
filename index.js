ead4783ea019710fc01366323e12d8a8d14aa14e
Patrick Pissurno <contato@patrickpissurno.com.br>
2018-07-05 07:17:34 -0300

    Change port and IP

--- index.js
+++ index.js
@@ -27,7 +27,7 @@
 
     fastify.register(require('./api'), { prefix: 'api' });
 
-    fastify.listen(8090, (err, address) => {
+    fastify.listen(80, (err, address) => {
         if (err)
             throw err;
         fastify.log.info(`Server listening on ${address}`);
